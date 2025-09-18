fe2caead38623c6755538ce864998945289d97d4
Joaquín Nahuel Conforti <confortijoaquin095@gmail.com>
2025-09-18 01:59:20 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "todo-app-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
+];
+
+// Instalar y cachear
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Servir desde caché
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Actualizar caché
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+});
